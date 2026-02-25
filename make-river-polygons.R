ae3fefcf9c9dd357fbf070395216e8b99c4f2464
Helen Miller <helen@ohana.org>
2026-02-24 16:39:13 -0800

    config for conus

--- make-river-polygons.R
+++ make-river-polygons.R
@@ -40,6 +40,8 @@ if (!file.exists(grwl_basin_vrt)) {
     basin_tiles <- st_intersection(grwl_tiles, basin)
     tile_ids <- basin_tiles$TILE_ID
     files <- file.path(grwl_mask_path, paste0(tile_ids, ".tif"))
+    # Tiles without any rivers are excluded, I think (e.g. NI10)
+    files <- files[file.exists(files)]
     rast_list <- lapply(files, rast)
     # project them all
     # This takes a few minutes but should make everything else 
@@ -57,7 +59,7 @@ if (!file.exists(grwl_basin_vrt)) {
 
 reaches <- st_transform(reaches, crs)
 geoms <- st_geometry(reaches)
-geoms_buff <- lapply(1:nrow(reaches), \(i) {
+geoms_buff <- pbapply::pblapply(1:nrow(reaches), \(i) {
     # maximum width is half the reach length. 
     width = min(as.numeric(st_length(reaches[i, ])/2), reaches$width[i]+2*sqrt(reaches$width_var[i]))
     st_buffer(geoms[i], width)
@@ -96,26 +98,26 @@ get_polygon <- function(i) {
     # 180 = Lake/reservoir
     # 126 = Tidal rivers/delta
     # 86  = Canal
-    reach_mask <- (water_mask %in% c(80, 180, 255, 126)) & (closest_r == reach_id)
+    reach_mask <- (water_mask %in% c(86, 180, 255, 126)) & (closest_r == reach_id)
     # make polygon
     p <- as.polygons(reach_mask)
     p_ <- p[p[[1]][[1]] == 1, ] #Only include "true" polygon (it also makes an inverse of "false" values)
 }
 
 polygon_list <- pbapply::pblapply(1:nrow(reaches), get_polygon)
-polygon_list <- lapply(polygon_list, st_as_sf)
+polygon_list <- pbapply::pblapply(polygon_list, st_as_sf)
 water_polygons <- Reduce(rbind, polygon_list)
 write_sf(water_polygons, file.path(basin_dir, "water_polygons.gpkg"))
 
-## make a quick figure for general exam thing
-# first need a bbox to include
-# 46.520432, -119.450801, 45.896930, -118.718603
-bounds <- data.frame(lon = c(-118.718603, -119.450801),
-                   lat = c(46.520432, 45.896930)) |>
-  st_as_sf(coords = c("lon", "lat"), 
-           crs = 4326) |>
-  st_bbox() |>
-  st_as_sfc()
-d <- st_intersection(water_polygons, st_transform(bounds, st_crs(water_polygons)))
-ggplot(d) + 
-    geom_sf(fill = rep_len(c("green", "blue"), 25))
+# ## make a quick figure for general exam thing
+# # first need a bbox to include
+# # 46.520432, -119.450801, 45.896930, -118.718603
+# bounds <- data.frame(lon = c(-118.718603, -119.450801),
+#                    lat = c(46.520432, 45.896930)) |>
+#   st_as_sf(coords = c("lon", "lat"), 
+#            crs = 4326) |>
+#   st_bbox() |>
+#   st_as_sfc()
+# d <- st_intersection(water_polygons, st_transform(bounds, st_crs(water_polygons)))
+# ggplot(d) + 
+#     geom_sf(fill = rep_len(c("green", "blue"), 25))
